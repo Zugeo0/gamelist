8109c678b8838dbb3b322a6e0898ada57928e94b
Zugeo <axelatlason2003@gmail.com>
2024-04-05 03:09:18 +0000

    Navbar cleanup

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
     import Navbar from "./components/navbar/Navbar.svelte";
+    // TODO: Remove
+    console.clear()
+
+    let navbarElements = [
+        {
+            icon: "ion:game-controller",
+            text: "Active Games",
+        },
+        {
+            label: "Games",
+            icon: "lucide:list-start",
+            text: "Game Lists",
+        },
+        {
+            icon: "bxs:calendar",
+            text: "Upcoming Games",
+        },
+        {
+            icon: "fa6-solid:dumpster",
+            text: "Backlog",
+        },
+        {
+            icon: "icons8:idea",
+            text: "Suggestions",
+        },
+        {
+            label: "Archives",
+            icon: "fluent-mdl2:completed-solid",
+            text: "Completions",
+        },
+    ];
 </script>
 
 <main class="flex flex-row absolute inset-0">
-    <Navbar />
+    <Navbar elements={navbarElements} />
 </main>
--- frontend/src/components/navbar/Navbar.svelte
+++ frontend/src/components/navbar/Navbar.svelte
@@ -4,42 +4,38 @@
     import NavbarElement from "./NavbarElement.svelte";
     import NavbarSeparator from "./NavbarSeparator.svelte";
 
+    interface NavElementData {
+        icon: string,
+        text: string,
+        label?: string,
+    };
+
     let selection = 0;
 
-    function onSelect(e: CustomEvent<number>) {
-        selection = e.detail;
+    export let elements: NavElementData[];
+
+    function setSelection(sel: number) {
+        selection = sel;
     }
 </script>
 
 <div class="h-full gap-2 max-w-80 w-1/3 bg-base overflow-hidden overflow-y-scroll border-r-overlay0 border-r flex flex-col justify-start items-center p-8">
+    <!-- Header -->
     <img src={logo} class="w-[80%] my-4" alt="Svelte Logo" />
 
-    <NavbarElement id={0} {selection} on:selected={onSelect}
-        icon="ion:game-controller"
-        label="Active Games"
-    />
+    <!-- Elements -->
+    {#each elements as element, id}
 
-    <NavbarSeparator label="Games" />
-    <NavbarElement id={1} {selection} on:selected={onSelect}
-        icon="lucide:list-start"
-        label="Game Lists"
-    />
-    <NavbarElement id={2} {selection} on:selected={onSelect}
-        icon="bxs:calendar"
-        label="Upcoming Games"
-    />
-    <NavbarElement id={3} {selection} on:selected={onSelect}
-        icon="fa6-solid:dumpster"
-        label="Backlog"
-    />
-    <NavbarElement id={4} {selection} on:selected={onSelect}
-        icon="icons8:idea"
-        label="Suggestions"
-    />
+        <!-- Create a label above the list element if it's specified -->
+        {#if element.label !== undefined}
+            <NavbarSeparator label={element.label} />
+        {/if}
 
-    <NavbarSeparator label="Archives" />
-    <NavbarElement id={5} {selection} on:selected={onSelect}
-        icon="fluent-mdl2:completed-solid"
-        label="Completions"
-    />
+        <NavbarElement
+            on:click={() => setSelection(id)}
+            selected={selection === id}
+            icon={element.icon}
+            label={element.text}
+        />
+    {/each}
 </div>
